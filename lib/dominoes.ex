8257c93c4e752bef9163151d8e8ae8e8e5345066
fabianherrera@gmail.com <admnts9000!>
2018-03-14 11:52:59 -0400

    chore: first commit to initialize github repo

---
+++ lib/dominoes.ex
@@ -0,0 +1,18 @@
+defmodule Dominoes do
+  @moduledoc """
+  Documentation for Dominoes.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Dominoes.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/dominoes_test.exs
@@ -0,0 +1,8 @@
+defmodule DominoesTest do
+  use ExUnit.Case
+  doctest Dominoes
+
+  test "greets the world" do
+    assert Dominoes.hello() == :world
+  end
+end
